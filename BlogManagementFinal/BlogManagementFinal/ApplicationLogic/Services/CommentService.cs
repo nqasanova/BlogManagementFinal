60d3bb919ad4541ecd27af51bb644f28a8448ffb
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-08-14 01:35:27 +0400

    Comment Service created and added to the system.

--- BlogManagementFinal/BlogManagementFinal/ApplicationLogic/Services/CommentService.cs
+++ BlogManagementFinal/BlogManagementFinal/ApplicationLogic/Services/CommentService.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using BlogManagementFinal.ApplicationLogic.Validations;
+
 namespace BlogManagementFinal.ApplicationLogic.Services
 {
     public class CommentService
     {
-        public CommentService()
+        public static string GetComment()
         {
+            bool isExceptionValid;
+            string comment = null;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Enter title : ");
+                    comment = Console.ReadLine();
+
+                    if (comment == "null")
+                    {
+                        throw new Exception();
+                    }
+
+                    isExceptionValid = false;
+                }
+
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong...");
+                    isExceptionValid = true;
+                }
+
+            } while (isExceptionValid || !Validation.IsLengthBetween(comment, 10, 35));
+
+            return comment;
         }
     }
 }
