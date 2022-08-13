e5c0ededb71268194a333a5fd67af0abc6810a40
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-08-14 01:30:47 +0400

    Comment Repository completed and added to the system.

--- BlogManagementFinal/BlogManagementFinal/Database/Repository/CommentRepository.cs
+++ BlogManagementFinal/BlogManagementFinal/Database/Repository/CommentRepository.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using BlogManagementFinal.Database.Models;
+
 namespace BlogManagementFinal.Database.Repository
 {
-    public class CommentRepository
+    public class CommentRepository : Common.Repository<Comment, int>
     {
-        public CommentRepository()
+        static CommentRepository()
         {
+            BlogRepository blogRepository = new BlogRepository();
+
+            DbContext.Add(new Comment(blogRepository.Get(b => b.Id == "BL64677"),
+                UserRepository.GetUserByEmail("natavan@gmail.com"),
+                "I hosnestly recomment this tutorial to everyone."));
+            DbContext.Add(new Comment(blogRepository.Get(b => b.Id == "BL75692"),
+                UserRepository.GetUserByEmail("natavan@gmail.com"),
+                "Teacher, can you help me pls?"));
+            DbContext.Add(new Comment(blogRepository.Get(b => b.Id == "BL46534"),
+                UserRepository.GetUserByEmail("natavan@gmail.com"),
+                "I want start java programming language."));
+        }
+
+        public static Comment Add(Blog blog, User sender, string text)
+        {
+            Comment comment = new Comment(blog, sender, text);
+            DbContext.Add(comment);
+            return comment;
         }
     }
 }
