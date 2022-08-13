8286ea393757200be1f053e63c3810370556164f
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-08-14 01:37:08 +0400

    Base Validation completed and added to the system.

--- BlogManagementFinal/BlogManagementFinal/ApplicationLogic/Validations/Validation.cs
+++ BlogManagementFinal/BlogManagementFinal/ApplicationLogic/Validations/Validation.cs
@@ -3,8 +3,9 @@ namespace BlogManagementFinal.ApplicationLogic.Validations
 {
     public class Validation
     {
-        public Validation()
+        public static bool IsLengthBetween(string text, int start, int end)
         {
+            return text.Length >= start && text.Length <= end;
         }
     }
 }
