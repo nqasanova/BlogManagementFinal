f2214bc16f5123a8d24ed63e78bfc9198931a525
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-08-14 01:27:24 +0400

    Comment Method completed and added to the system.

--- BlogManagementFinal/BlogManagementFinal/Database/Models/Comment.cs
+++ BlogManagementFinal/BlogManagementFinal/Database/Models/Comment.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using BlogManagementFinal.Database.Models.Common;
+using BlogManagementFinal.Database.Repository;
+
 namespace BlogManagementFinal.Database.Models
 {
-    public class Comment
+    public class Comment : Entity<int>
     {
-        public Comment()
+        public Blog Blog { get; set; }
+        public User Sender { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedTime { get; set; }
+
+        public Comment(Blog blog, User sender, string content, int? id = null)
         {
+            Blog = blog;
+            Sender = sender;
+            Content = content;
+            CreatedTime = DateTime.Now;
+
+            if (id != null)
+            {
+                Id = id.Value;
+            }
+
+            else
+            {
+                Id = UserRepository.IdCounter;
+            }
+        }
+
+        public string GetCommentInfo()
+        {
+            return CreatedTime.ToString("mm.dd.yyyy") + " " + Sender.FirstName + " " + Sender.LastName + " " + Content;
         }
     }
 }
