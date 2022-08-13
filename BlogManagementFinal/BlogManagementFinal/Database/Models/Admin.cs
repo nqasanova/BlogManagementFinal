7788de055602187479c395dca001f16592436f04
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-08-14 01:26:04 +0400

    Admin Model xompleted and added to the system.

--- BlogManagementFinal/BlogManagementFinal/Database/Models/Admin.cs
+++ BlogManagementFinal/BlogManagementFinal/Database/Models/Admin.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 namespace BlogManagementFinal.Database.Models
 {
-    public class Admin
+    public class Admin : User
     {
-        public Admin()
+        public Admin(string firstName, string lastName, string email, string password, int id)
+            : base(firstName, lastName, email, password, id) //adding user after deleting
         {
+
+
+        }
+
+        public Admin(string firstName, string lastName, string email, string password)
+            : base(firstName, lastName, email, password) //adding user
+        {
+
+
+        }
+
+        public Admin(string firstName, string lastName)
+            : base(firstName, lastName) //for updating
+        {
+
+
+        }
+
+        public override string GetUserInfo()
+        {
+            return $"First Name : {FirstName}, Last Name : {LastName}, Email : {Email}, Registration Date : {RegistrationDate}";
         }
     }
 }
